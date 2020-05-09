7f82952bacd17e60fc95fad677702e5490c8e600
Luis Londono <lhl49@cornell.edu>
2020-05-09 16:09:36 -0400

    linked buttons to onscrolls on design page

--- js/design.js
+++ js/design.js
@@ -6,23 +6,23 @@ var rightsideInfoContainerRect;
 var scrollInterval = 0;
 
 function pageSpecificOnLoad() {
-  console.log("sponsors window.onload()...");
+  console.log("design window.onload()...");
   infoContainers = {
     all: {
       index: 0,
-      infoContainerID: "sponsors"
+      infoContainerID: "the-pod"
     },
     propulsion: {
       index: 1,
-      infoContainerID: "sponsor-packages"
+      infoContainerID: "propulsion"
     },
     braking: {
       index: 2,
-      infoContainerID: "sponsors"
+      infoContainerID: "braking"
     },
     suspension: {
       index: 3,
-      infoContainerID: "sponsors"
+      infoContainerID: "suspension"
     },
     
   };
@@ -30,7 +30,7 @@ function pageSpecificOnLoad() {
   addEventListenersToLabels();
   console.log("Adding event listeners to labels...");
 
-  infoContainerKeyInViewCache = "sponsors";
+  infoContainerKeyInViewCache = "all";
   let infoContainerKeyInViewCacheRect = document
     .getElementById(
       infoContainers[infoContainerKeyInViewCache]["infoContainerID"]
@@ -49,8 +49,85 @@ function pageSpecificOnLoad() {
   // console.log("infoContainerKeyInViewCacheCenterX: " + infoContainerKeyInViewCacheCenterX)
 }
 
+function addEventListenersToLabels() {
+  for (const key in infoContainers) {
+    console.log(key);
+    if (infoContainers.hasOwnProperty(key)) {
+      let label = getLabelElement(key);
+      label.addEventListener("click", function() {
+        scrollToContainer(key);
+      });
+    }
+  }
+}
+
+function scrollToContainer(infoContainerKey) {
+  infoContainer = infoContainers[infoContainerKey];
+
+  document
+    .getElementById(infoContainer["infoContainerID"])
+    .scrollIntoView({ behavior: "smooth" });
+
+  document.getElementById("active-label").id = "inactive-label";
+
+  getLabelElement(infoContainerKey).id = "active-label";
+}
+
 
 function getLabelElement(nameOfKey) {
   labelIndex = infoContainers[nameOfKey]["index"];
   return document.getElementsByClassName("sidebar-list-label")[labelIndex];
+}
+function updateLabelInView() {
+  if (window.innerWidth < tabletMaxWidth) {
+    return;
+  }
+  infoContainerKeyInViewCacheRect = document
+    .getElementById(
+      infoContainers[infoContainerKeyInViewCache]["infoContainerID"]
+    )
+    .getBoundingClientRect();
+  infoContainerInViewCenterX =
+    infoContainerKeyInViewCacheRect.top +
+    infoContainerKeyInViewCacheRect.height / 2;
+  // console.log("infoContainerKeyInViewCacheCenterX: " + infoContainerKeyInViewCacheCenterX)
+  // console.log("infoContainerInViewCenterX: " + infoContainerInViewCenterX)
+  delta = Math.abs(
+    infoContainerInViewCenterX - infoContainerKeyInViewCacheCenterX
+  );
+  // console.log("Delta: " + delta)
+
+  if (delta > infoContainerKeyInViewCacheRect.height * 0.3) {
+    // console.log("Movement > .45 detected")
+    infoContainerInViewport = getinfoContainerKeyInView();
+    infoContainerChanged =
+      infoContainerKeyInViewCache != infoContainerInViewport;
+
+    if (infoContainerChanged) {
+      infoContainerKeyInViewCache = infoContainerInViewport;
+      // console.log("infoContainer in viewport changed to: " + infoContainerInViewport)
+      document.getElementById("active-label").id = "inactive-label";
+      getLabelElement(infoContainerInViewport).id = "active-label";
+    }
+  }
+}
+function getinfoContainerKeyInView() {
+  rightsideInfoContainerCenterX =
+    rightsideInfoContainerRect.top + rightsideInfoContainerRect.height / 2;
+
+  for (const key in infoContainers) {
+    if (infoContainers.hasOwnProperty(key)) {
+      let infoContainerContainerRect = document
+        .getElementById(infoContainers[key]["infoContainerID"])
+        .getBoundingClientRect();
+      infoContainerContainerCenterX =
+        infoContainerContainerRect.top + infoContainerContainerRect.height / 2;
+      if (
+        rightsideInfoContainerRect.top < infoContainerContainerCenterX &&
+        infoContainerContainerCenterX < rightsideInfoContainerRect.bottom
+      ) {
+        return key;
+      }
+    }
+  }
 }
