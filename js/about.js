3393f1f9d23343de376578d4d4ce0b80e303799d
cziffMan <ad4680@gmail.com>
2021-05-07 12:56:04 -0400

    About page modification, persistent error with slideshow not loading straight away

--- js/about.js
+++ js/about.js
@@ -6,7 +6,7 @@ var logo_symbol;
 function pageSpecificOnLoad(){
   console.log("Loading about")
   window.onscroll = function() { handleWindowScroll() }
-  
+
   header = document.getElementsByTagName('header')[0]
   logo_symbol = document.getElementsByClassName("logo-symbol")[0]
   // sticky = header.offsetHeight * .9;
@@ -27,4 +27,30 @@ function handleWindowScroll(){
   } else {
     header.classList.remove("sticky-header");
   }
+}
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
 }
