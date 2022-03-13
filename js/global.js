e27a5ea055f7928e0d9618f1a128318a2fd6926e
Aditya Deshpande <adityadeshpande@Adityas-MacBook-Air.local>
2022-03-13 11:39:58 -0400

    Giving Day Countdown Timer Added

--- js/global.js
+++ js/global.js
@@ -11,6 +11,31 @@ pageType = {
   sponsors: 4
 };
 
+var countDownDate = new Date("March 16, 2022 11:59:00").getTime();
+var x = setInterval(function() {
+
+  // Get today's date and time
+  var now = new Date().getTime();
+
+  // Find the distance between now and the count down date
+  var distance = countDownDate - now;
+
+  // Time calculations for days, hours, minutes and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  // Display the result in the element with id="demo"
+  document.getElementById("giv-day-id").innerHTML = days + "d " + hours + "h "+ minutes + "m " + seconds + "s ";
+
+  // If the count down is finished, write some text
+  if (distance < 0) {
+      clearInterval(x);
+      document.getElementById("giv-day-id").innerHTML = "Giving day is TODAY";
+  }
+}, 1000);
+
 function getPageType() {
   var path = window.location.pathname;
   if (path == "/") {
